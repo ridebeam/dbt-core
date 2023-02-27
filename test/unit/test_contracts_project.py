db84daae5a991d0d1cc87c698a1926ea7d390479
Dave Connors <dave.connors@fishtownanalytics.com>
2023-02-27 09:27:11 -0600

    update old test with non int value

--- test/unit/test_contracts_project.py
+++ test/unit/test_contracts_project.py
@@ -42,6 +42,7 @@ class TestProject(ContractTestCase):
             'version': '1.0',
             'profile': 'test',
             'project-root': '/usr/src/app',
+            'config-version': 'non-int',
         }
         with self.assertRaises(Exception):
             self.ContractType.from_dict(dct)
